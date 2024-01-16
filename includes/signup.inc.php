ecf290c0cfe4cf69520a0bbc23adf8a66e6a94f4
Ignatiusdork <littignatius@gmail.com>
2024-01-16 15:47:38 +0100

    signup-input-completed commit

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -5,5 +5,58 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+    $email = $_POST['email'];
+
+    try {
+
+        require_once 'dbh.inc.php';
+        require_once 'signup_model.inc.php';
+        require_once 'signup_contr.inc.php';
+
+        // error handlers
+        $errors = [];
     
+        if (is_input_empty($username, $pwd, $email)) {
+            $errors["empty_inputs"] = 'Fill in all fields';
+        }
+
+        if (is_email_invalid($email)) {
+            $errors["invalid email"] = "Invalid email used";
+        }
+
+        if (is_username_taken($pdo, $username)) {
+            $errors["username_used"] = "Username already taken";
+        }
+
+        if (is_email_registered($pdo, $email)) {
+            $errors["email_ised"] = "Email already registered";
+        }
+
+        require_once 'config_sessions.inc.php';
+
+        //check if there are any errors
+        if ($errors) {
+            $_SESSION['errors_signup'] = $errors;
+            header("Location: ../index.php");
+            die();
+        }
+
+        // create the user and store in the database
+        create_user($pdo, $pwd, $username, $email);
+        header("Location: ../index.php?signup=success");
+
+        //kill the conection to the database
+        $pdo = null;
+        $stmt = null;
+        die();
+
+    } catch(PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+
+} else {
+    header("Location: ../index.php");
+    die();
 }
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'includes/config_sessions.inc.php';
+require_once 'includes/signup_view.inc.php';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,6 +17,20 @@
     </head>
 
     <body>
-        
+        <h3>Signup</h3>
+
+        <div>
+            <form action="includes/signup.inc.php" method="post">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="pwd" placeholder="password">
+                <input type="text" name="email" placeholder="E-mail">
+                <button>Signup</button>
+            </form>
+        </div>
+
+        <?php 
+        check_signup_errors();
+        ?>
+
     </body>
 </html>
